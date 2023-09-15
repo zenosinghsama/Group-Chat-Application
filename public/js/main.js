8a5ddc646abede946656adb58e596be335742dee
zenosinghsama <iamarjunsingh007@gmail.com>
2023-09-15 16:51:07 +0530

    Multiple Groups

--- public/js/main.js
+++ public/js/main.js
@@ -1,119 +1,105 @@
 const messageInput = document.getElementById("message");
 const sendBtn = document.getElementById("send");
-const chatMsg = document.getElementById("chat-messages");
+const chatBox = document.getElementById("chatBox");
+const groupDropDown = document.getElementById("groupDropDown");
 
-document.addEventListener("DOMContentLoaded", async() => {
+let token = localStorage.getItem("Token");
+let decodedToken = parseJwt(token);
+let userName = decodedToken.name;
+let selectedGroup = null;
+
+// DOM CONTENT
+document.addEventListener("DOMContentLoaded", async () => {
     try {
         const token = localStorage.getItem("Token");
 
-        if(!token) {
-            return (window.location.href = "/login.html");     
+        if (!token) {
+            return (window.location.href = "/login.html");
         }
 
-        const storedMessages = JSON.parse(localStorage.getItem("chatMessages")) || [];
-        const recentMessages = storedMessages.slice(-MaxChatMsg);
-
-        recentMessages.forEach((messageObj) => {
-            const messageText = messageObj.message;
-            const isSentByCurrentUser = messageObj.userId === localStorage.getItem("ID");
-            showMessageOnScreen(messageText, isSentByCurrentUser);
-        });
+        const joinMessage = `${userName} joined the chat`;
+        showMessageOnScreen(joinMessage);
 
         await fetchAndDisplayMsg();
-    } catch(err) {
+    } catch (err) {
         console.log("NO TOKEN FOUND", err);
     }
 });
 
 
-//FETCH MESSAGE FROM DB
+// FETCH MESSAGE FROM DB
 async function fetchAndDisplayMsg() {
     try {
         const token = localStorage.getItem("Token");
 
         const response = await axios.get("/messages", {
-            headers : {
-                Authorization : `${token}`
-            }
+            headers: {
+                Authorization: `${token}`,
+            },
         });
 
-        const newMessages = response.data.allMessages.filter((messageObj) => {
-            return !isMessageInLocalStorage(messageObj);
-        });
+        const newMessages = response.data.allMessages;
 
         newMessages.forEach((messageObj) => {
+            const senderName = messageObj.name;
+            console.log("SENDER NAME ",senderName);
             const messageText = messageObj.message;
-            const isSentByCurrentUser = messageObj.userId === localStorage.getItem("ID");
-            showMessageOnScreen(messageText, isSentByCurrentUser);
-
-            addToLocalStorage(messageObj);
-        }) ;
-    } catch (err) {
-        console.log("ERROR IN FETCHING MESSAGE", err);
-    }
-}
-
-async function fetchAndDisplayNewMsg() {
-    try {
-        // ...
-
-        newMessages.forEach((messageObj) => {
-            const messageText = messageObj.message;
-            const isSentByCurrentUser = messageObj.userId === localStorage.getItem("ID");
-            showMessageOnScreen(messageText, isSentByCurrentUser);
-
-            // Add the new message to local storage
-            addToLocalStorage(messageObj);
+            const isSentByCurrentUser =
+                messageObj.userId === localStorage.getItem("ID");
+            showMessageOnScreen(senderName, messageText, isSentByCurrentUser);
         });
     } catch (err) {
         console.log("ERROR IN FETCHING MESSAGE", err);
     }
 }
 
-// setInterval(fetchAndDisplayMsg, 1000);
-const MaxChatMsg = 10;
-function isMessageInLocalStorage(messageObj) {
-    const storedMessages = JSON.parse(localStorage.getItem("chatMessages")) || [];
 
-    if(storedMessages.length >= MaxChatMsg) {
-        storedMessages.shift();
-    }
-
-    storedMessages.push(messageObj);
-    localStorage.setItem("chatMessages", JSON.stringify(storedMessages));
-}
-
-function addToLocalStorage(messageObj) {
-    const storedMessages = JSON.parse(localStorage.getItem("chatMessages")) || [];
-    storedMessages.push(messageObj);
-    localStorage.setItem("chatMessages", JSON.stringify(storedMessages));
-}
-
-//SHOW MSG ON SCREEN
-function showMessageOnScreen(message, isSentByCurrentUser) {
-    const username = localStorage.getItem("name");
-    const chatBox = document.getElementById("chatBox");
+// SHOW MSG ON SCREEN
+function showMessageOnScreen(senderName, message, messageType ,isSentByCurrentUser) {
     const messageContainer = document.createElement("div");
-    messageContainer.className =  isSentByCurrentUser ? "message sent" : "message received";
+    messageContainer.className = "message";
 
-    const userNameSpan = document.createElement("span");
-    userNameSpan.className = "username";
-    userNameSpan.textContent = username;
+    const messageText = document.createElement("div");
+    messageText.textContent = message;
 
     const messageBubble = document.createElement("div");
     messageBubble.textContent = message;
 
-    messageContainer.appendChild(userNameSpan);
-    messageContainer.appendChild(messageBubble);
+    if(messageType === "join") {
+        const joinMessage = document.createElement("div");
+        joinMessage.className = "join-message";
+        joinMessage.textContent = `${senderName} joined the chat`
 
+        messageContainer.appendChild(joinMessage);
+    } else {
+
+        if (isSentByCurrentUser) {
+        const userNameSpan = document.createElement("span");
+        userNameSpan.className = "username current-user";
+        userNameSpan.textContent = userName;
+
+        messageContainer.appendChild(userNameSpan);
+    } else {
+        const senderUserName = document.createElement("div");
+        senderUserName.className = "username";
+        senderUserName.textContent = senderName;
+
+        messageContainer.appendChild(senderUserName);
+    }
+    }
+    messageContainer.appendChild(messageText);
     chatBox.appendChild(messageContainer);
 }
 
- //SAVE TO DB
- async function saveToDB(message) {
+// SAVE TO DB
+async function saveToDB(message) {
+
+    if (message === "User joined the chat") {
+        message = `${userName} joined the chat`;
+    }
 
     const data = {
-        message : message
+        message: message
     };
 
     try {
@@ -121,28 +107,51 @@ function showMessageOnScreen(message, isSentByCurrentUser) {
 
         const response = await axios.post("/sendMessage", data, {
             headers: {
-                Authorization : `${token}`,
-            }
+                Authorization: `${token}`,
+            },
         });
 
         messageInput.value = "";
 
         const newMessage = response.data.newAddedMessage.message;
-        showMessageOnScreen(newMessage, true);
-
-        addToLocalStorage(response.data.newAddedMessage);
-        
+        const name = response.data.newAddedMessage.name;
+  
+        showMessageOnScreen(name, newMessage, true);
     } catch (err) {
         alert("ERROR IN SAVING MESSAGE TO DB");
         console.log(err);
     }
 }
 
-sendBtn.addEventListener("click", function() {
+async function joinChat() {
+    try {
+        const joinMessage = `${userName} joined the chat`;
+
+        await saveToDB(joinMessage, "join");
+    } catch(err) {
+        console.log("ERROR JOINING CHAT", err);
+    }
+}
+
+sendBtn.addEventListener("click", function () {
     const message = messageInput.value.trim();
-    if(message !== "") {
-        saveToDB(message);
+    if (message !== "") {
+        saveToDB(message, "message");
     }
 });
 
-
+//PARSE TOKEN 
+function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  }
